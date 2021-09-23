4ccb847d698ac1f1c9a426b0a92d516b4e6934ac
waydk <waydk.main@gmail.com>
2021-09-23 20:54:51 +0800

    0.0.4: search photo

--- anisource/__main__.py
+++ anisource/__main__.py
@@ -1,21 +1,39 @@
-from flask import Flask, render_template
 from PicImageSearch import SauceNAO
 from environs import Env
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+# SET YOUR FOLDER
+UPLOAD_FOLDER = '/home/waydk/python_projects/anisource-web/anisource/img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def home():
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('search',
+                                    filename=filename))
     return render_template("index.html")
 
-@app.route("/search")
-def search():
+@app.route('/search/<filename>')
+def search(filename):
     env = Env()
     env.read_env()
 
     api_key = env.str("SAUCE_API")
     saucenao = SauceNAO(api_key=api_key)
-    res = saucenao.search('https://trace.moe/img/tinted-good.jpg')
+    res = saucenao.search(f"anisource/img/{filename}")
     try:
         est_time = f"☁ Est Time: {res.origin['results'][0]['data']['est_time']}\n"
     except KeyError:
@@ -35,7 +53,6 @@ def search():
               f"{part}" \
               f"☁ Url: {res.url}"
     return message
-
 
 if __name__ == '__main__':
     app.run(debug=True)
