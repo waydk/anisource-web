12391666d1c932a787b767c8c1139ce958734235
waydk <waydk.main@gmail.com>
2021-09-26 21:32:32 +0800

    0.0.7: search by link

--- anisource/__main__.py
+++ anisource/__main__.py
@@ -3,37 +3,25 @@ from environs import Env
 import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+import base64
 
 # SET YOUR FOLDER
 UPLOAD_FOLDER = '/home/waydk/python_projects/anisource-web/anisource/img'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('search',
-                                    filename=filename))
-    return render_template("index.html")
-
-@app.route('/search/<filename>')
-def search(filename):
+def saucenao_search(filename=None, link=None):
     env = Env()
     env.read_env()
 
     api_key = env.str("SAUCE_API")
     saucenao = SauceNAO(api_key=api_key)
-    res = saucenao.search(f"anisource/img/{filename}")
+    if filename:
+        res = saucenao.search(f"anisource/img/{filename}")
+    else:
+        res = saucenao.search(link)
     try:
         est_time = res.origin['results'][0]['data']['est_time']
     except KeyError:
@@ -54,6 +42,37 @@ def search(filename):
         photo=res.thumbnail,
         similarity=res.similarity
     )
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        text = request.form['text']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('search_by_image',
+                                    filename=filename))
+        elif text:
+            bytes_text = base64.b64encode(text.encode("UTF-8"))
+            decoded_text = bytes_text.decode("utf-8")
+            return redirect(url_for('search_by_link', link=decoded_text))
+
+    return render_template("index.html")
+
+@app.route('/search_image/<filename>')
+def search_by_image(filename):
+    return saucenao_search(filename=filename)
+
+@app.route('/search_link/<link>')
+def search_by_link(link):
+    bytes_text = base64.b64decode(link.encode("utf-8"))
+    link = bytes_text.decode('utf-8')
+    return saucenao_search(link=link)
 
 
 if __name__ == '__main__':
